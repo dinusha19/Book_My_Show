b841c0c6563d4730cb42615b34897e5dc6bb4f97
dinusha19 <sdinusha842@gmail.com>
2021-08-26 21:03:39 +0530

    added entertainment carousel

--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import HeroSlider from "react-slick";
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
+//Components
+import {NextArrow,PrevArrow} from "./Arrows.components";
 
 const HeroCarousel = () =>{
 
@@ -13,9 +13,9 @@ const HeroCarousel = () =>{
         slidesToShow: 1,
         autoplay:true,
         infinite:true,
-        slidesToScroll: 1
-        
-
+        slidesToScroll: 1,
+        nextArrow:<NextArrow/>,
+        prevArrow:<PrevArrow/>,
     };
     const settings = {
         arrows:true,
@@ -24,14 +24,16 @@ const HeroCarousel = () =>{
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        nextArrow:<NextArrow/>,
+        prevArrow:<PrevArrow/>,
       };
 
-      const images = ["https://images.unsplash.com/photo-1593642533144-3d62aa4783ec?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-      "https://images.unsplash.com/photo-1629740081942-29c155e9aeb3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-      "https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=828&q=80",
-      "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80",
-      "https://images.unsplash.com/photo-1629757349708-a9abe5d3d354?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80"];
+      const images = ["https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80",
+      "https://images.unsplash.com/photo-1613380007620-b9612888117f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1616527546362-bf6b7f80a751?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80",
+      "https://images.unsplash.com/photo-1615986200762-a1ed9610d3b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=751&q=80",
+      "https://images.unsplash.com/photo-1585064788609-b216a8e6613b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"];
     return (
     <>
     <div className="lg:hidden">
